41d538e11cd4ccc1685311e72caa64f805684326
Michel Ortega <coolnerdcool@gmail.com>
2020-07-26 20:29:06 -0500

    FEAT: Create more basic files, then implement those.

---
+++ greawo/Presenter/HomePresenter.swift
@@ -0,0 +1,18 @@
+//
+//  HomePresenter.swift
+//  greawo
+//
+//  Created by Michel Ortega on 7/26/20.
+//  Copyright © 2020 Michel Ortega. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HomePresenter {
+    /* THIS CLASS IS THE PRESENTER */
+//    MARK: - Properties.
+    
+//    MARK: - Methods.
+    
+}
